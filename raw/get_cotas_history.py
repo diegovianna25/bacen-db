05fc549d226c67664585b54a3aa4f176e1175b42
diegovianna25 <diegovianna25>
2024-04-20 20:24:35 +0000

    limpeza do código

--- raw/get_cotas_history.py
+++ raw/get_cotas_history.py
@@ -7,6 +7,7 @@ import zipfile
 import io
 from datetime import datetime
 from dateutil.parser import isoparse
+from pyspark.sql import SparkSession
 
 # COMMAND ----------
 
@@ -25,67 +26,6 @@ from dateutil.parser import isoparse
 #dbutils.fs.ls("/mnt/datalake/raw/")
 
 # COMMAND ----------
-
-def obter_dados_cotas_ano(ano):
-    formato = "json"
-    url = f"http://www.camara.leg.br/cotas/Ano-{ano}.{formato}.zip"
-    
-    # Fazer o download do arquivo ZIP
-    response = requests.get(url)
-    
-    # Verificar se a solicitação foi bem-sucedida (código de status 200)
-    if response.status_code == 200:
-        # Ler o conteúdo do arquivo ZIP
-        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
-            # Iterar sobre os arquivos no ZIP
-            for filename in zip_file.namelist():
-                # Verificar se o arquivo é do ano atual ou anterior
-                if filename.startswith(f"Ano-{ano}."):
-                    # Extrair e carregar os dados do arquivo JSON em um DataFrame
-                    with zip_file.open(filename) as json_file:
-                        df = pd.read_json(json_file)
-                    
-                    # Expandir os dados da coluna "dados"
-                    df_expandido = pd.json_normalize(df['dados'])
-                    
-                    # Converter a coluna "dataEmissao" para o tipo datetime
-                    df_expandido["dataEmissao"] = pd.to_datetime(df_expandido["dataEmissao"])
-                    
-                    # Salvar os dados em um arquivo local
-                    salvar_dados_local(df_expandido, ano)
-                    
-                    return df_expandido
-        
-        print(f"Nenhum dado disponível para o ano {ano}.")
-        return None
-    else:
-        print(f"Erro ao baixar o arquivo ZIP para o ano {ano}.")
-        return None
-
-def salvar_dados_local(df, ano):
-    # Salvar os dados em um arquivo local
-    df.to_json(f"dados_{ano}.json", orient="records")
-
-# Baixar dados para os anos de 2023 até o mês anterior ao atual
-ano_atual = datetime.now().year
-mes_atual = datetime.now().month
-
-for ano in range(2023, ano_atual):
-    print(f"Obtendo dados para o ano {ano}...")
-    obter_dados_cotas_ano(ano)
-
-# Para o ano atual, baixar apenas até o mês anterior ao atual
-print(f"Obtendo dados para o ano {ano_atual} até o mês {mes_atual - 1}...")
-obter_dados_cotas_ano(ano_atual)
-
-# COMMAND ----------
-
-import requests
-import zipfile
-import io
-import datetime
-import pandas as pd
-from pyspark.sql import SparkSession
 
 def baixar_dados_cotas_ano(ano):
     formato = "json"
